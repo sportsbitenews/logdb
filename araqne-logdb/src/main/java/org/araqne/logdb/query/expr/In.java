d87047f50ba23343793a74686e7a3b26f05e7f93
Won-gune Hwang <stania.pe.kr@gmail.com>
2013-03-27 16:47:26 +0900

    added String matcher accelerater.

--- araqne-logdb/src/main/java/org/araqne/logdb/query/expr/In.java
+++ araqne-logdb/src/main/java/org/araqne/logdb/query/expr/In.java
@@ -28,23 +28,75 @@ public class In implements Expression {
 	}
 
 	private static class StringMatcher extends FieldMatcher {
+		public static enum StringMatchMethod {
+			EQUALS,
+			STARTSWITH,
+			ENDSWITH,
+			CONTAINS,
+			PATTERN,
+		}
+
 		private String term;
+		private String operand;
 		private Pattern pattern;
+		private StringMatchMethod matchMethod;
 
 		public StringMatcher(String s) {
 			this.term = s;
-			pattern = Eq.tryBuildPattern2(term);
+			int count = countOfAsterisk(term);
+			int first = term.indexOf('*');
+			int last = term.lastIndexOf('*');
+			if (count == 1 && first == 0) {
+				matchMethod = StringMatchMethod.ENDSWITH;
+				operand = term.substring(1);
+			} else if (count == 1 && last == term.length() - 1) {
+				matchMethod = StringMatchMethod.STARTSWITH;
+				operand = term.substring(0, last);
+			} else if (count == 2 && first == 0 && last == term.length() - 1) {
+				matchMethod = StringMatchMethod.CONTAINS;
+				operand = term.substring(1, last);
+			} else {
+				pattern = Eq.tryBuildPattern2(term);
+				if (pattern != null)
+					matchMethod = StringMatchMethod.PATTERN;
+				else
+					matchMethod = StringMatchMethod.EQUALS;
+			}
 		}
 
 		public boolean match(LogMap log, Object o) {
 			if (o instanceof CharSequence) {
-				if (pattern != null)
-					return pattern.matcher((CharSequence) o).matches();
-				else
-					return term.equals(o);
+				String token = o.toString();
+				switch (matchMethod) {
+				case EQUALS:
+					return token.equals(term);
+				case STARTSWITH:
+					return token.startsWith(operand);
+				case ENDSWITH:
+					return token.endsWith(operand);
+				case CONTAINS:
+					return token.contains(operand);
+				case PATTERN:
+					return pattern.matcher(token).matches();
+				default:
+					throw new IllegalStateException("bad match method: " + matchMethod.toString());
+				}
 			} else {
 				return false;
 			}
+		}
+		
+		private int countOfAsterisk(String term2) {
+			int cnt = 0;
+			int start = -1;
+			while (true) {
+				int p = term2.indexOf('*', start + 1);
+				if (p == -1)
+					break;
+				cnt ++;
+				start = p;
+			}
+			return cnt;
 		}
 	}
 
