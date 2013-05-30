197ed1fe270e96696431fec149f9c39f055e76c8
xeraph <xeraph@nchovy.com>
2013-05-30 22:10:46 +0900

    closed araqne/logpresso.com#112. support secui mf2 syslog format

---
+++ araqne-syslog-parser/src/main/java/org/araqne/logparser/syslog/secui/Mf2LogParserFactory.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2013 Eediom Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.araqne.logparser.syslog.secui;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Locale;
+import java.util.Map;
+
+import org.apache.felix.ipojo.annotations.Component;
+import org.apache.felix.ipojo.annotations.Provides;
+import org.araqne.log.api.LogParser;
+import org.araqne.log.api.LogParserFactory;
+import org.araqne.log.api.LoggerConfigOption;
+
+/**
+ * @since 1.9.2
+ * @author xeraph
+ * 
+ */
+@Component(name = "mf2-log-parser-factory")
+@Provides
+public class Mf2LogParserFactory implements LogParserFactory {
+
+	@Override
+	public String getName() {
+		return "mf2";
+	}
+
+	@Override
+	public Collection<Locale> getDisplayNameLocales() {
+		return Arrays.asList(Locale.ENGLISH);
+	}
+
+	@Override
+	public String getDisplayName(Locale locale) {
+		return "MF2";
+	}
+
+	@Override
+	public Collection<Locale> getDescriptionLocales() {
+		return Arrays.asList(Locale.ENGLISH);
+	}
+
+	@Override
+	public String getDescription(Locale locale) {
+		return "SECUI MF2";
+	}
+
+	@Override
+	public Collection<LoggerConfigOption> getConfigOptions() {
+		return new ArrayList<LoggerConfigOption>();
+	}
+
+	@Override
+	public LogParser createParser(Map<String, String> config) {
+		return new Mf2LogParser();
+	}
+}
