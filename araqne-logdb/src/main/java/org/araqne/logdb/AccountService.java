331e98f5225dcbed44c8950392d8a5dd52484c2b
xeraph <xeraph@nchovy.com>
2013-06-28 00:26:40 +0900

    added @since annotation

--- araqne-logdb/src/main/java/org/araqne/logdb/AccountService.java
+++ araqne-logdb/src/main/java/org/araqne/logdb/AccountService.java
@@ -23,6 +23,9 @@ public interface AccountService {
 
 	Session getSession(String guid);
 
+	/**
+	 * @since 1.2.5
+	 */
 	Session newSession(String loginName);
 
 	Session login(String loginName, String hash, String nonce);
