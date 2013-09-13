ad232b15ae87a5d80e3a1235de3105a127b6734d
xeraph <xeraph@nchovy.com>
2013-09-13 16:43:12 +0900

    fixed araqne/igloosec#2. fixed inner join bug, and added hash join optimization

--- araqne-logdb/src/main/java/org/araqne/logdb/query/command/Join.java
+++ araqne-logdb/src/main/java/org/araqne/logdb/query/command/Join.java
@@ -2,6 +2,7 @@ package org.araqne.logdb.query.command;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -19,6 +20,15 @@ public class Join extends LogQueryCommand {
 	private Result subQueryResult;
 	private LogResultSet subQueryResultSet;
 
+	// for later sort-merge join
+	private Object[] sortJoinKeys1;
+	private Object[] sortJoinKeys2;
+
+	// for hash join
+	private HashMap<JoinKeys, List<Object>> hashJoinMap;
+	private JoinKeys joinKeys;
+
+	private int joinKeyCount;
 	private SortField[] sortFields;
 	private List<LogQueryCommand> subQuery;
 
@@ -26,6 +36,11 @@ public class Join extends LogQueryCommand {
 
 	public Join(SortField[] sortFields, List<LogQueryCommand> subQuery) {
 		try {
+			this.joinKeyCount = sortFields.length;
+			this.joinKeys = new JoinKeys(new Object[joinKeyCount]);
+			this.sortJoinKeys1 = new Object[sortFields.length];
+			this.sortJoinKeys2 = new Object[sortFields.length];
+
 			this.sortFields = sortFields;
 			this.subQuery = subQuery;
 			this.subQueryResult = new Result("sub");
@@ -74,36 +89,46 @@ public class Join extends LogQueryCommand {
 		}
 
 		if (subQueryResultSet == null) {
-			try {
-				subQueryResultSet = subQueryResult.getResult();
-				logger.debug("araqne logdb: fetch subquery result of query [{}:{}]", logQuery.getId(), logQuery.getQueryString());
-			} catch (IOException e) {
-				logger.error("araqne logdb: cannot get subquery result of query " + logQuery.getId(), e);
-			}
-		} else {
-			subQueryResultSet.reset();
+			eof(true);
+			return;
 		}
 
-		ArrayList<Object> joinKeys = new ArrayList<Object>();
+		subQueryResultSet.reset();
+
+		if (hashJoinMap != null) {
+			int i = 0;
+			for (SortField f : sortFields)
+				joinKeys.keys[i++] = m.get(f.getName());
+
+			List<Object> l = hashJoinMap.get(joinKeys);
+			if (l == null)
+				return;
+
+			for (Object o : l) {
+				@SuppressWarnings("unchecked")
+				Map<String, Object> sm = (Map<String, Object>) o;
+				Map<String, Object> joinMap = new HashMap<String, Object>(m.map());
+				joinMap.putAll(sm);
+				write(new LogMap(joinMap));
+			}
+			return;
+		}
+
+		int i = 0;
 		for (SortField f : sortFields)
-			joinKeys.add(m.get(f.getName()));
+			sortJoinKeys1[i++] = m.get(f.getName());
 
 		while (subQueryResultSet.hasNext()) {
 			Map<String, Object> sm = subQueryResultSet.next();
-			int i = 0;
-			for (SortField f : sortFields) {
-				Object v1 = sm.get(f.getName());
-				Object v2 = joinKeys.get(i);
 
-				if (v1 == null && v2 == null) {
-					Map<String, Object> joinMap = new HashMap<String, Object>(m.map());
-					joinMap.putAll(sm);
-					write(new LogMap(joinMap));
-				} else if (v1 != null && v2 != null && v1.equals(v2)) {
-					Map<String, Object> joinMap = new HashMap<String, Object>(m.map());
-					joinMap.putAll(sm);
-					write(new LogMap(joinMap));
-				}
+			i = 0;
+			for (SortField f : sortFields)
+				sortJoinKeys2[i++] = sm.get(f.getName());
+
+			if (Arrays.equals(sortJoinKeys1, sortJoinKeys2)) {
+				Map<String, Object> joinMap = new HashMap<String, Object>(m.map());
+				joinMap.putAll(sm);
+				write(new LogMap(joinMap));
 			}
 		}
 	}
@@ -137,6 +162,8 @@ public class Join extends LogQueryCommand {
 	}
 
 	private class SubQueryRunner implements Runnable {
+		private static final int HASH_JOIN_THRESHOLD = 50000;
+
 		@Override
 		public void run() {
 			try {
@@ -145,6 +172,17 @@ public class Join extends LogQueryCommand {
 
 				subQuery.get(0).eof(false);
 				LogQueryCommand cmd = subQuery.get(subQuery.size() - 1);
+
+				try {
+					subQueryResultSet = subQueryResult.getResult();
+					logger.debug("araqne logdb: fetch subquery result of query [{}:{}]", logQuery.getId(), logQuery.getQueryString());
+
+					if (subQueryResultSet.size() <= HASH_JOIN_THRESHOLD)
+						buildHashJoinTable();
+
+				} catch (IOException e) {
+					logger.error("araqne logdb: cannot get subquery result of query " + logQuery.getId(), e);
+				}
 
 				synchronized (cmd) {
 					cmd.notifyAll();
@@ -155,6 +193,53 @@ public class Join extends LogQueryCommand {
 				logger.debug("araqne logdb: subquery end, query " + logQuery.getId());
 			}
 		}
+
+		private void buildHashJoinTable() {
+			hashJoinMap = new HashMap<JoinKeys, List<Object>>(50000);
+
+			while (subQueryResultSet.hasNext()) {
+				Map<String, Object> sm = subQueryResultSet.next();
+
+				Object[] keys = new Object[joinKeyCount];
+				for (int i = 0; i < joinKeyCount; i++)
+					keys[i] = sm.get(sortFields[i].getName());
+
+				JoinKeys joinKeys = new JoinKeys(keys);
+				List<Object> l = hashJoinMap.get(joinKeys);
+				if (l == null) {
+					l = new ArrayList<Object>(2);
+					hashJoinMap.put(joinKeys, l);
+				}
+
+				l.add(sm);
+			}
+		}
 	}
 
+	private static class JoinKeys {
+		private Object[] keys;
+
+		public JoinKeys(Object[] keys) {
+			this.keys = keys;
+		}
+
+		@Override
+		public int hashCode() {
+			return Arrays.hashCode(keys);
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			JoinKeys other = (JoinKeys) obj;
+			if (!Arrays.equals(keys, other.keys))
+				return false;
+			return true;
+		}
+	}
 }
