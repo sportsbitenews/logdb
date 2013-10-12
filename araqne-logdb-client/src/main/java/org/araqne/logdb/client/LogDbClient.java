4794be2cd5a40348dbba9be29470789205a6df63
xeraph <xeraph@nchovy.com>
2013-10-13 00:29:49 +0900

    closed araqne/igloosec#17. support client locale for logger, parser, transformer display name and description

--- araqne-logdb-client/src/main/java/org/araqne/logdb/client/LogDbClient.java
+++ araqne-logdb-client/src/main/java/org/araqne/logdb/client/LogDbClient.java
@@ -25,6 +25,7 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.NoSuchElementException;
@@ -50,6 +51,7 @@ public class LogDbClient implements TrapListener {
 	private LogDbSession session;
 	private int fetchSize = 10000;
 	private Map<Integer, LogQuery> queries = new HashMap<Integer, LogQuery>();
+	private Locale locale = Locale.getDefault();
 
 	public LogDbClient() {
 		this(new WebSocketTransport());
@@ -57,6 +59,15 @@ public class LogDbClient implements TrapListener {
 
 	public LogDbClient(LogDbTransport transport) {
 		this.transport = transport;
+	}
+
+	public Locale getLocale() {
+		return locale;
+	}
+
+	public void setLocale(Locale locale) {
+		checkNotNull("locale", locale);
+		this.locale = locale;
 	}
 
 	/**
@@ -511,7 +522,10 @@ public class LogDbClient implements TrapListener {
 
 	@SuppressWarnings("unchecked")
 	public List<LoggerFactoryInfo> listLoggerFactories() throws IOException {
-		Message resp = session.rpc("org.araqne.log.api.msgbus.LoggerPlugin.getLoggerFactories");
+		Map<String, Object> params = new HashMap<String, Object>();
+		params.put("locale", locale.getLanguage());
+
+		Message resp = session.rpc("org.araqne.log.api.msgbus.LoggerPlugin.getLoggerFactories", params);
 
 		List<LoggerFactoryInfo> factories = new ArrayList<LoggerFactoryInfo>();
 		List<Object> l = (List<Object>) resp.get("factories");
@@ -539,6 +553,7 @@ public class LogDbClient implements TrapListener {
 
 		Map<String, Object> params = new HashMap<String, Object>();
 		params.put("factory", factoryName);
+		params.put("locale", locale.getLanguage());
 		Message resp2 = session.rpc("org.araqne.log.api.msgbus.LoggerPlugin.getFactoryOptions", params);
 		List<ConfigSpec> configSpecs = parseConfigList((List<Object>) resp2.get("options"));
 		found.setConfigSpecs(configSpecs);
@@ -561,7 +576,10 @@ public class LogDbClient implements TrapListener {
 
 	@SuppressWarnings("unchecked")
 	public List<ParserFactoryInfo> listParserFactories() throws IOException {
-		Message resp = session.rpc("org.araqne.log.api.msgbus.LoggerPlugin.getParserFactories");
+		Map<String, Object> params = new HashMap<String, Object>();
+		params.put("locale", locale.getLanguage());
+		
+		Message resp = session.rpc("org.araqne.log.api.msgbus.LoggerPlugin.getParserFactories", params);
 		List<Object> l = (List<Object>) resp.get("factories");
 
 		List<ParserFactoryInfo> parsers = new ArrayList<ParserFactoryInfo>();
@@ -676,7 +694,10 @@ public class LogDbClient implements TrapListener {
 
 	@SuppressWarnings("unchecked")
 	public List<TransformerFactoryInfo> listTransformerFactories() throws IOException {
-		Message resp = session.rpc("org.logpresso.core.msgbus.TransformerPlugin.listTransformerFactories");
+		Map<String, Object> params = new HashMap<String, Object>();
+		params.put("locale", locale.getLanguage());
+
+		Message resp = session.rpc("org.logpresso.core.msgbus.TransformerPlugin.listTransformerFactories", params);
 		List<Object> l = (List<Object>) resp.get("factories");
 
 		List<TransformerFactoryInfo> factories = new ArrayList<TransformerFactoryInfo>();
@@ -698,6 +719,7 @@ public class LogDbClient implements TrapListener {
 	public TransformerFactoryInfo getTransformerFactoryInfo(String name) throws IOException {
 		Map<String, Object> params = new HashMap<String, Object>();
 		params.put("factory_name", name);
+		params.put("locale", locale.getLanguage());
 
 		Message resp = session.rpc("org.logpresso.core.msgbus.TransformerPlugin.getTransformerFactoryInfo", params);
 		Map<String, Object> m = (Map<String, Object>) resp.get("factory");
