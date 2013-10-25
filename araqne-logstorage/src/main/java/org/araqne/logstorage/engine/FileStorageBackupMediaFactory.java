64f194a49ad55c6fde944b17c5b7de1686c59673
xeraph <xeraph@nchovy.com>
2013-10-25 17:48:02 +0900

    added license terms

--- araqne-logstorage/src/main/java/org/araqne/logstorage/engine/FileStorageBackupMediaFactory.java
+++ araqne-logstorage/src/main/java/org/araqne/logstorage/engine/FileStorageBackupMediaFactory.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2013 Eediom Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.araqne.logstorage.engine;
 
 import java.io.File;
@@ -17,6 +32,11 @@ import org.araqne.logstorage.backup.StorageBackupMedia;
 import org.araqne.logstorage.backup.StorageBackupMediaFactory;
 import org.araqne.logstorage.backup.StorageBackupMediaRegistry;
 
+/**
+ * @since 2.3.0
+ * @author xeraph
+ * 
+ */
 @Component(name = "logstorage-file-backup-media-factory")
 public class FileStorageBackupMediaFactory implements StorageBackupMediaFactory {
 
--- araqne-logstorage/src/main/java/org/araqne/logstorage/engine/StorageBackupMediaRegistryImpl.java
+++ araqne-logstorage/src/main/java/org/araqne/logstorage/engine/StorageBackupMediaRegistryImpl.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2013 Eediom Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.araqne.logstorage.engine;
 
 import java.util.ArrayList;
@@ -9,6 +24,11 @@ import org.apache.felix.ipojo.annotations.Provides;
 import org.araqne.logstorage.backup.StorageBackupMediaFactory;
 import org.araqne.logstorage.backup.StorageBackupMediaRegistry;
 
+/**
+ * @since 2.3.0
+ * @author xeraph
+ * 
+ */
 @Component(name = "logstorage-backup-media-registry")
 @Provides
 public class StorageBackupMediaRegistryImpl implements StorageBackupMediaRegistry {
