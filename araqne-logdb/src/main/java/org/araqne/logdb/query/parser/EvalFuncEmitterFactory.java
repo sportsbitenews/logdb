e6b15f5163def57f3fca7ec2b5a1b97e26467723
xeraph <xeraph@eediom.com>
2014-02-08 01:01:50 +0900

    removed immature timeunity func

--- araqne-logdb/src/main/java/org/araqne/logdb/query/parser/EvalFuncEmitterFactory.java
+++ araqne-logdb/src/main/java/org/araqne/logdb/query/parser/EvalFuncEmitterFactory.java
@@ -102,8 +102,6 @@ public class EvalFuncEmitterFactory implements FuncEmitterFactory {
 			exprStack.add(new Split(args));
 		} else if (name.equals("array")) {
 			exprStack.add(new Array(args));
-		} else if (name.equals("timeunity")) {
-			exprStack.add(new TimeUnity(args));
 		} else {
 			throw new QueryParseException("unsupported-function", -1, "function name is " + name);
 		}
