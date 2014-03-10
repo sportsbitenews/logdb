7093d83fb1fc4002cb96deea8ebe09d6e34c1370
everclear-- <faditia@gmail.com>
2014-03-10 17:02:50 +0900

    implemented LogFileV3Reader

---
+++ araqne-logstorage/src/test/java/org/araqne/logstorage/file/LogFileV3Walker.java
@@ -0,0 +1,24 @@
+package org.araqne.logstorage.file;
+
+import java.io.IOException;
+
+import org.araqne.logstorage.file.LogFileV3Reader.LogBlockV3;
+import org.araqne.storage.api.FilePath;
+import org.araqne.storage.localfile.LocalFilePath;
+
+public class LogFileV3Walker {
+	public static void main(String[] args) throws IOException {
+		FilePath indexPath = new LocalFilePath(args[0]);
+		FilePath dataPath = indexPath.getAbsoluteFilePath().getParentFilePath().newFilePath(indexPath.getName().replace(".idx", ".dat"));
+		LogFileV3Reader reader = new LogFileV3Reader(indexPath, dataPath, null, null);
+		
+		System.out.printf("%s blocks\n", reader.getBlockCount());
+		while (reader.hasNextBlock()) {
+			LogBlockV3 block = reader.nextBlock();
+			System.out.printf("logId : %s ~ %s\n", block.getMinId(), block.getMaxId());
+		}
+		
+		reader.close();
+	}
+
+}
