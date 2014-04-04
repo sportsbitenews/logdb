7a37e81e8d9c271bd0dd904c83654d487ebe57fc
stania (Won-gune Hwang) <stania.pe.kr@gmail.com>
2014-04-04 20:38:20 +0900

    fixed getDay implementation (regarding DST)

--- araqne-logstorage/src/main/java/org/araqne/logstorage/LogUtil.java
+++ araqne-logstorage/src/main/java/org/araqne/logstorage/LogUtil.java
@@ -1,25 +1,51 @@
 package org.araqne.logstorage;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.TimeZone;
 
 public class LogUtil {
+	public static void main(String[] args) throws ParseException {
+		long currentTimeMillis = System.currentTimeMillis();
+		System.out.println(getDay(new Date()));
+		for (int i = 0; i < 10000000; ++i) {
+			Date day = getDay(new Date());
+		}
+		System.out.println(System.currentTimeMillis() - currentTimeMillis);
+		System.out.println(getDay(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse("1987-05-26 03:00:00")));
+		currentTimeMillis = System.currentTimeMillis();
+		for (int i = 0; i < 10000000; ++i) {
+			Date day = getDay2(new Date());
+		}
+		System.out.println(System.currentTimeMillis() - currentTimeMillis);
+		System.out.println(getDay2(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse("1987-05-26 03:00:00")));
+	}
+	
 	private static ThreadLocal<TimeZone> timeZoneCache = new ThreadLocal<TimeZone>() {
 		@Override
 		protected TimeZone initialValue() {
 			return TimeZone.getDefault();
 		}
 	};
-	
+
 	public static Date getDay(Date date) {
 		long time = date.getTime();
 		TimeZone timeZone = timeZoneCache.get();
 		if (timeZone.inDaylightTime(date))
-			return new Date(time - ((time + timeZone.getRawOffset() + timeZone.getDSTSavings()) % 86400000L));
+			return new Date(time - ((time + timeZone.getRawOffset() + 3600000) % 86400000L));
 		else
 			return new Date(time - ((time + timeZone.getRawOffset()) % 86400000L));
+	}
+	
+	private static int timezoneOffset = Calendar.getInstance().getTimeZone().getRawOffset();
+	@Deprecated
+	public static Date getDay2(Date date) {
+		long time = date.getTime();
+		return new Date(time - ((time + timezoneOffset) % 86400000L));
 	}
 
 	public static Map<String, Object> newLogData(String ... strings) {
