eb684470f50273d8785ecea238a85d9dce85d218
xeraph <xeraph@eediom.com>
2014-05-27 10:09:40 +0900

    fixed jython localization

--- araqne-logdb-jython/src/main/java/org/araqne/logdb/jython/impl/JythonLoggerScriptRegistryImpl.java
+++ araqne-logdb-jython/src/main/java/org/araqne/logdb/jython/impl/JythonLoggerScriptRegistryImpl.java
@@ -93,11 +93,13 @@ public class JythonLoggerScriptRegistryImpl extends AbstractLoggerFactory implem
 
 	@Override
 	public String getDisplayName(Locale locale) {
-		if (locale != null && locale.equals(Locale.CHINESE)) 
-			return "jython数据采集器";
-		return "jython logger";
+		if (locale != null && locale.equals(Locale.CHINESE))
+			return "Jython数据采集器";
+		if (locale != null && locale.equals(Locale.KOREAN))
+			return "자이썬 수집기";
+		return "Jython Logger";
 	}
-	
+
 	@Override
 	public Collection<Locale> getDisplayNameLocales() {
 		return Arrays.asList(Locale.ENGLISH, Locale.CHINESE);
@@ -106,10 +108,12 @@ public class JythonLoggerScriptRegistryImpl extends AbstractLoggerFactory implem
 	@Override
 	public String getDescription(Locale locale) {
 		if (locale != null && locale.equals(Locale.CHINESE))
-			return "thon数据采集器脚本";
-		return "jython logger script";
+			return "Jython数据采集器脚本";
+		if (locale != null && locale.equals(Locale.KOREAN))
+			return "자이썬 수집 스크립트";
+		return "Jython Logger Script";
 	}
-	
+
 	@Override
 	public Collection<Locale> getDescriptionLocales() {
 		return Arrays.asList(Locale.ENGLISH, Locale.CHINESE);
@@ -117,19 +121,18 @@ public class JythonLoggerScriptRegistryImpl extends AbstractLoggerFactory implem
 
 	@Override
 	public Collection<LoggerConfigOption> getConfigOptions() {
-		LoggerConfigOption scriptName = new StringConfigType("logger_script", map("logger script name", "数据采集器脚本名称"),
-				map("jython logger script class name", "数据采集器脚本类名称"), true);
+		LoggerConfigOption scriptName = new StringConfigType("logger_script", map("logger script name", "스크립트 이름", "数据采集器脚本名称"),
+				map("jython logger script class name", "스크립트 클래스 이름", "数据采集器脚本类名称"), true);
 		return Arrays.asList(scriptName);
 	}
 
-	private Map<Locale, String> map(String value, String cn) {
+	private Map<Locale, String> map(String value, String ko, String cn) {
 		Map<Locale, String> m = new HashMap<Locale, String>();
 		m.put(Locale.ENGLISH, value);
+		m.put(Locale.KOREAN, ko);
 		m.put(Locale.CHINESE, cn);
 		return m;
 	}
-	
-
 
 	@Override
 	protected Logger createLogger(LoggerSpecification spec) {
