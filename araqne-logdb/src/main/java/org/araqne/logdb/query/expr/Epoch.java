0372e53a50c17c536f8304f3980b568c2277e314
xeraph <xeraph@eediom.com>
2014-12-28 22:21:57 +0900

    closed araqne/issue#949. support automatic epoch conversion regardless of time unit (second or millisecond)

--- araqne-logdb/src/main/java/org/araqne/logdb/query/expr/Epoch.java
+++ araqne-logdb/src/main/java/org/araqne/logdb/query/expr/Epoch.java
@@ -12,7 +12,7 @@ public class Epoch extends FunctionExpression {
 
 	public Epoch(QueryContext ctx, List<Expression> exprs) {
 		super("epoch", exprs, 1);
-		
+
 		this.valueExpr = exprs.get(0);
 	}
 
@@ -26,8 +26,17 @@ public class Epoch extends FunctionExpression {
 			String s = value.toString();
 			if (s.isEmpty())
 				return null;
-			
-			return new Date(Long.valueOf(s) * 1000);
+
+			long time = Long.valueOf(s);
+
+			// assume time as millisecond
+			// if x1000 is larger than 9999-01-01
+			long ms = time * 1000;
+
+			if (ms >= 253402300799000L)
+				return new Date(time);
+
+			return new Date(ms);
 		} catch (Throwable t) {
 			return null;
 		}
