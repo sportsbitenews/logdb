c779516e5ba2d53a65db3900499a1ae68a217bc5
Jun Gyoung Seong <jungyoung.seong@gmail.com>
2015-03-09 21:36:37 +0900

    Implement StreamJoin.

---
+++ araqne-logdb/src/main/java/org/araqne/logdb/query/command/StreamJoin.java
@@ -0,0 +1,146 @@
+package org.araqne.logdb.query.command;
+
+import java.io.IOException;
+
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane.SystemMenuBar;
+
+import org.araqne.cron.AbstractTickTimer;
+import org.araqne.cron.TickService;
+import org.araqne.logdb.Query;
+import org.araqne.logdb.QueryCommand;
+import org.araqne.logdb.QueryResultSet;
+import org.araqne.logdb.QueryService;
+import org.araqne.logdb.QueryStopReason;
+import org.araqne.logdb.QueryTask;
+import org.araqne.logdb.Row;
+import org.araqne.logdb.impl.QueryHelper;
+import org.araqne.logdb.query.command.Join.JoinType;
+import org.araqne.logdb.query.command.Sort.SortField;
+import org.araqne.logdb.query.command.SortMergeJoiner;
+import org.araqne.logdb.query.command.StreamSortMergeJoiner.SortMergeJoinerListener;
+import org.araqne.logdb.query.engine.QueryTaskRunner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class StreamJoin extends QueryCommand {
+	private final Logger logger = LoggerFactory.getLogger(StreamJoin.class);
+	private final JoinType joinType;
+	private final SortField[] sortFields;
+	private final Query subQuery;
+	private final StreamSortMergeJoiner sortMergeJoiner;
+
+	private SubQueryTask subQueryTask = new SubQueryTask();
+	long lastFlushedTime;
+
+	QueryService queryService;
+	
+	public StreamJoin(JoinType joinType, SortField[] sortFields, Query subQuery, TickService tickService, QueryService queryService) {
+		this.joinType = joinType;
+		this.sortFields = sortFields;
+		this.subQuery = subQuery;
+		this.queryService = queryService;
+
+		this.sortMergeJoiner = new StreamSortMergeJoiner(joinType, sortFields, new SortMergeJoinerCallback(this), tickService);
+
+		lastFlushedTime = System.currentTimeMillis();
+
+		logger.debug("araqne logdb: join subquery created [{}:{}]", subQuery.getId(), subQuery.getQueryString());
+
+		QueryHelper.setJoinAndUnionDependencies(subQuery.getCommands());
+
+		subQueryTask.run();
+	}
+
+	@Override
+	public String getName() {
+		return "Stream Join";
+	}
+
+	@Override
+	public void onStart() {
+		subQuery.preRun();
+	}
+
+	@Override
+	public void onClose(QueryStopReason reason) {
+		if (reason == QueryStopReason.PartialFetch || reason == QueryStopReason.End) {
+			synchronized (sortMergeJoiner) {
+				sortMergeJoiner.close();
+			}
+		} else {
+			try {
+				synchronized (sortMergeJoiner) {
+					sortMergeJoiner.cancel();
+				}
+			} catch (Throwable t) {
+				logger.error("araqne logdb: can not cancel sortMergeJoiner", t);
+			}
+		}
+
+		try {
+			subQuery.stop(reason);
+		} catch (Throwable t) {
+			logger.error("araqne logdb: cannot stop subquery [" + subQuery.getQueryString() + "]", t);
+		} finally {
+			subQuery.purge();
+		}
+	}
+
+	@Override
+	public void onPush(Row m) {
+		try {
+			synchronized (sortMergeJoiner) {
+				sortMergeJoiner.setR(m);
+			}
+		} catch (Throwable t) {
+			logger.error("araqne logdb: cannot setR on sortMergeJoiner[" + m.toString() + "]", t);
+		}
+	}
+
+	class SortMergeJoinerCallback implements SortMergeJoinerListener {
+		StreamJoin join;
+
+		SortMergeJoinerCallback(StreamJoin join) {
+			this.join = join;
+		}
+
+		@Override
+		public void onPushPipe(Row row) {
+			join.pushPipe(row);
+		}
+	}
+
+	private class SubQueryTask extends QueryTask {
+		@Override
+		public void run() {
+			logger.debug("logpresso query: StreamJoin's SubQueryTask run [{}]", subQuery.getQueryString());
+
+			QueryResultSet rs = null;
+			try {
+				Query query = queryService.createQuery(subQuery.getContext().getSession(), subQuery.getQueryString());
+				queryService.startQuery(query.getId());
+				do {
+					try {
+						Thread.sleep(50);
+					} catch (InterruptedException e) {
+					}
+				} while (!query.isFinished());
+					
+				rs = query.getResultSet();
+
+				// logger.debug("logpresso query: StreamJoin fetch subquery result of query [{}:{}]",
+				// query.getId(), query.getQueryString());
+				synchronized (sortMergeJoiner) {
+					sortMergeJoiner.setS(rs);
+				}
+			} catch (IOException e) {
+				logger.error("logpresso query: cannot get subquery result of query " + query.getId(), e);
+			} finally {
+				if (rs != null) {
+					rs.close();
+				}
+			}
+		}
+	}
+
+}
---
+++ araqne-logdb/src/main/java/org/araqne/logdb/query/parser/StreamJoinParser.java
@@ -0,0 +1,74 @@
+package org.araqne.logdb.query.parser;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import org.araqne.cron.TickService;
+import org.araqne.logdb.AbstractQueryCommandParser;
+import org.araqne.logdb.DefaultQuery;
+import org.araqne.logdb.Query;
+import org.araqne.logdb.QueryCommand;
+import org.araqne.logdb.QueryCommandPipe;
+import org.araqne.logdb.QueryContext;
+import org.araqne.logdb.QueryParserService;
+import org.araqne.logdb.QueryResultFactory;
+import org.araqne.logdb.QueryService;
+import org.araqne.logdb.query.command.Join;
+import org.araqne.logdb.query.command.Join.JoinType;
+import org.araqne.logdb.query.command.Sort;
+import org.araqne.logdb.query.command.Sort.SortField;
+import org.araqne.logdb.query.command.StreamJoin;
+
+public class StreamJoinParser extends AbstractQueryCommandParser {
+	private QueryParserService parserService;
+	private QueryService queryService;
+	private TickService tickService;
+	private QueryResultFactory resultFactory;
+
+	public StreamJoinParser(QueryParserService parserService, TickService tickService, QueryResultFactory resultFactory, QueryService queryService) {
+		this.parserService = parserService;
+		this.tickService = tickService;
+		this.resultFactory = resultFactory;
+		this.queryService = queryService;
+	}
+
+	@Override
+	public String getCommandName() {
+		return "streamjoin";
+	}
+
+	@Override
+	public QueryCommand parse(QueryContext context, String commandString) {
+		int b = commandString.indexOf('[');
+		int e = commandString.lastIndexOf(']');
+
+		int cmdLen = getCommandName().length();
+		String fieldToken = commandString.substring(cmdLen, b);
+		String subQueryString = commandString.substring(b + 1, e).trim();
+
+		ParseResult r = QueryTokenizer.parseOptions(context, fieldToken, 0, Arrays.asList("type"), getFunctionRegistry());
+		@SuppressWarnings("unchecked")
+		Map<String, Object> options = (Map<String, Object>) r.value;
+
+		String type = null;
+		if (options != null) {
+			type = (String) options.get("type");
+		}
+
+		if (r.next < 0)
+			r.next = 0;
+
+		JoinType joinType = JoinType.Inner;
+		if (type != null && type.equals("left"))
+			joinType = JoinType.Left;
+
+		List<SortField> sortFields = SortField.parseSortFields(fieldToken, r);
+
+		SortField[] sortFieldArray = sortFields.toArray(new SortField[0]);
+		List<QueryCommand> subCommands = parserService.parseCommands(context, subQueryString);
+		Query subQuery = new DefaultQuery(context, subQueryString, subCommands, resultFactory);
+
+		return new StreamJoin(joinType, sortFieldArray, subQuery, tickService, queryService);
+	}
+}
