cfd152b5e5fa0a1f09d34cb2d9af73193dba2802
xeraph <xeraph@eediom.com>
2015-08-12 17:44:29 +0900

    fixed araqne/issue#1565. fixed query result file leak

--- araqne-logdb/src/main/java/org/araqne/logdb/query/engine/QueryResultImpl.java
+++ araqne-logdb/src/main/java/org/araqne/logdb/query/engine/QueryResultImpl.java
@@ -249,6 +249,13 @@ public class QueryResultImpl implements QueryResult, LogFlushCallback {
 
 		// delete files
 		synchronized (writerLock) {
+			try {
+				writer.close();
+			} catch (IOException e) {
+				int queryId = config.getQuery().getId();
+				logger.warn("araqne logdb: cannot close query [" + queryId + "] result writer", e);
+			}
+			
 			writer.purge();
 		}
 	}
