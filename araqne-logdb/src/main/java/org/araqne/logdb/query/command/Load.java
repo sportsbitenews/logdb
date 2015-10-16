5353b6b416dc4bcb0d0b97dab0b46f0c1d1d50dd
mindori <mindori@kaist.ac.kr>
2015-10-16 15:06:16 +0900

    fixed araqne/issue#1645. removed useless import statements

--- araqne-logdb/src/main/java/org/araqne/logdb/query/command/Load.java
+++ araqne-logdb/src/main/java/org/araqne/logdb/query/command/Load.java
@@ -1,8 +1,5 @@
 package org.araqne.logdb.query.command;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.araqne.logdb.DriverQueryCommand;
 import org.araqne.logdb.QueryTask.TaskStatus;
 import org.araqne.logdb.Row;
